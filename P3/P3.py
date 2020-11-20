b1a886a532fd9264b933c63e22f86e458381a815
Tim Paul <tmp2822d@vt.edu>
2020-11-19 21:26:38 -0500

    created a skeleton of the class for the commands

--- P3/P3.py
+++ P3/P3.py
@@ -1,1 +1,20 @@
 
+class Commands:
+
+    def Search():
+        return 0
+
+    def Remove():
+        return 0
+
+    def Merge():
+        return 0
+
+    def List():
+        return 0
+
+    def Copy():
+        return 0
+
+
+
